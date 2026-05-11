67d6a3da32e3eaa0f4632b28b35dc910968a0055
Sakura-Matsui <sakura.matsui@metateam.co.jp>
2026-05-11 17:03:25 +0900

    書籍登録機能の実装

--- src/main/java/jp/co/metateam/library/service/BookMstService.java
+++ src/main/java/jp/co/metateam/library/service/BookMstService.java
@@ -19,31 +19,49 @@ import jp.co.metateam.library.repository.BookMstRepository;
 public class BookMstService {
 
     private final BookMstRepository bookMstRepository;
-    
+
     @Autowired
-    public BookMstService(BookMstRepository bookMstRepository){
+    public BookMstService(BookMstRepository bookMstRepository) {
         this.bookMstRepository = bookMstRepository;
     }
-    
-    public List<BookMstDto> findAvailableWithStockCount() {
-        List<BookMst> books = this.bookMstRepository.findLimitedBook();
-        List<BookMstDto> bookMstDtoList = new ArrayList<BookMstDto>();
 
-        // 書籍の在庫数を取得
-        // FIXME: 現状は書籍ID毎にDBに問い合わせている。一度のSQLで完了させたい。
+    // 一覧表示
+    public List<BookMstDto> findAvailableWithStockCount() {
+
+        List<BookMst> books = this.bookMstRepository.findLimitedBook();
+
+        List<BookMstDto> bookMstDtoList = new ArrayList<>();
+
         for (int i = 0; i < books.size(); i++) {
+
             BookMst book = books.get(i);
-            BookMstDto bookMstDto = new BookMstDto();
-            bookMstDto.setId(book.getId());
-            bookMstDto.setIsbn(book.getIsbn());
-            bookMstDto.setTitle(book.getTitle());
-            bookMstDtoList.add(bookMstDto);
+
+            BookMstDto dto = new BookMstDto();
+
+            dto.setId(book.getId());
+            dto.setIsbn(book.getIsbn());
+            dto.setTitle(book.getTitle());
+
+            bookMstDtoList.add(dto);
         }
 
         return bookMstDtoList;
     }
-    
+
+    // ISBN検索（後で重複チェックに使う）
+    public BookMst findByIsbn(String isbn) {
+        return this.bookMstRepository.findByIsbn(isbn).orElse(null);
+    }
+
+    // 保存処理
+    @Transactional
+    public void save(BookMstDto bookMstDto) {
+
+        BookMst book = new BookMst();
+
+        book.setIsbn(bookMstDto.getIsbn());
+        book.setTitle(bookMstDto.getTitle());
+
+        this.bookMstRepository.save(book);
+    }
 }
-
-
-
