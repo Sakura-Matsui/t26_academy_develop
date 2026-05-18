e61bbd357e3a045a4578382328a211a2b95525ba
Sakura-Matsui <sakura.matsui@metateam.co.jp>
2026-05-18 19:21:07 +0900

    修正しました。

--- src/main/java/jp/co/metateam/library/controller/BookController.java
+++ src/main/java/jp/co/metateam/library/controller/BookController.java
@@ -17,7 +17,8 @@ import jp.co.metateam.library.model.BookMst;
 import jp.co.metateam.library.model.BookMstDto;
 import jp.co.metateam.library.service.BookMstService;
 import lombok.extern.log4j.Log4j2;
- 
+
+
 /**
  * 書籍関連クラス
  */
--- src/main/java/jp/co/metateam/library/model/BookMstDto.java
+++ src/main/java/jp/co/metateam/library/model/BookMstDto.java
@@ -24,7 +24,7 @@ public class BookMstDto {
 
     //ISBNのルール
     @NotEmpty(message = "ISBNは必須です")
-    @Size(max = 50, message = "ISBNは50桁以内で入力してください")
+    @Size(max = 13, message = "ISBNは13桁以内で入力してください")
     @Pattern(regexp = "^[0-9]+$", message = "ISBNは半角数字で入力してください")
     private String isbn; 
     
